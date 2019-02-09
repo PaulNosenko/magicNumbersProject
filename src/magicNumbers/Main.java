45bc8e53969eec8af5fd6d92eac3806122ac74af
Paul Nosenko <pavelgadzenko@gmail.com>
2019-02-09 13:07:03 +0100

    check if file exists

--- src/magicNumbers/Main.java
+++ src/magicNumbers/Main.java
@@ -1,15 +1,23 @@
 package magicNumbers;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Main {
 	private static Scanner sc;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		sc = new Scanner(System.in);
 		System.out.println("Please enter the path and the name of the file");
 		String fileName = sc.nextLine();
-		System.out.println(fileName);
+
+		File f = new File(fileName);
 		
+		if(f.exists()) {
+			byte[] b = Files.readAllBytes(f.toPath());
+		}else {
+			System.out.println("File '" + fileName + "' not found. Try once again");
+		}
 	}
 }
