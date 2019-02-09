abb15473753ef5ee863429b3dc18675da5aba7bc
Paul Nosenko <pavelgadzenko@gmail.com>
2019-02-09 13:16:51 +0100

    transforming file to HEX format

--- src/magicNumbers/Main.java
+++ src/magicNumbers/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.nio.file.Files;
 import java.util.Scanner;
 
+import magicNumbers.utils.Utility;
+
 public class Main {
 	private static Scanner sc;
 	public static void main(String[] args) throws Exception {
@@ -13,9 +15,16 @@ public class Main {
 		String fileName = sc.nextLine();
 
 		File f = new File(fileName);
+		String ext = Utility.getExtension(f);
+		StringBuilder sb = null;
 		
 		if(f.exists()) {
 			byte[] b = Files.readAllBytes(f.toPath());
+			sb = new StringBuilder();
+			for(int i=0; i<9; i++) {
+				sb.append(String.format("%02X", b[i]));
+			}
+			System.out.println(sb.toString());
 		}else {
 			System.out.println("File '" + fileName + "' not found. Try once again");
 		}
