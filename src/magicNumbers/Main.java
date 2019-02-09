6e72799c88f5322ef8985fdb7ae017db7e34e579
Paul Nosenko <pavelgadzenko@gmail.com>
2019-02-09 13:02:38 +0100

    user input for path entering added

--- src/magicNumbers/Main.java
+++ src/magicNumbers/Main.java
@@ -1,7 +1,15 @@
 package magicNumbers;
 
+import java.util.Scanner;
+
 public class Main {
+	private static Scanner sc;
 	public static void main(String[] args) {
+		
+		sc = new Scanner(System.in);
+		System.out.println("Please enter the path and the name of the file");
+		String fileName = sc.nextLine();
+		System.out.println(fileName);
 		
 	}
 }
